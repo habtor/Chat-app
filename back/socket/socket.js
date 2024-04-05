02b042f9305bdf996a2de3b680916796e9c22540
Saleh <39927806+habtor@users.noreply.github.com>
2024-04-05 20:04:42 +0200

    added functionality to show username when user is typing

--- back/socket/socket.js
+++ back/socket/socket.js
@@ -19,17 +19,32 @@ export const getReceiverSocketId = (receiverId) => {
 const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
-  console.log("a user connected", socket.id);
-
   const userId = socket.handshake.query.userId;
   if (userId != "undefined") userSocketMap[userId] = socket.id;
 
   // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
+   
+  socket.on("typing", (username) => {
+    // Broadcast 'typing' event to other clients
+    socket.broadcast.emit("typing", username);
+  });
+
+
+  // to target a specific user, we need to get the socket id of the target user
+  socket.on("typingToUser", (userData) => {
+    const { username, targetUser } = userData;
+    const targetSocketId = userSocketMap[targetUser];
+
+    if (targetSocketId) {
+      socket.to(targetSocketId).emit("typingNotification", username);
+    } else {
+      console.log(`User ${targetUser} is not connected.`);
+    }
+  });
 
   // socket.on() is used to listen to the events. can be used both on client and server side
   socket.on("disconnect", () => {
-    console.log("user disconnected", socket.id);
     delete userSocketMap[userId];
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
--- front/src/components/messages/MessageInput.jsx
+++ front/src/components/messages/MessageInput.jsx
@@ -1,10 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BsSend } from "react-icons/bs";
 import useSendMessage from "../../hooks/useSendMessage";
+import { useSocketContext } from "../../context/socketContext";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
 
 const MessageInput = () => {
+  const { socket } = useSocketContext();
+  const { authUser } = useAuthContext();
   const [message, setMessage] = useState("");
   const { loading, sendMessage } = useSendMessage();
+  const [userTyping, setUserTyping] = useState("");
+  const { selectedConversation } = useConversation();
+
+  const userWriting = (e) => {
+    const userMessage = e.target.value;
+    setMessage(userMessage);
+    const userData = {
+      username: authUser._id,
+      targetUser: selectedConversation?._id,
+    };
+    socket.emit("typingToUser", userData);
+  };
+
+  useEffect(() => {
+    console.log(userTyping);
+    const handleTypingNotification = (username) => {
+      setUserTyping(username);
+    };
+
+    socket.on("typingNotification", handleTypingNotification);
+    setTimeout(() => {
+      setUserTyping(null);
+    }, 3000);
+
+    return () => {
+      socket.off("typingNotification", handleTypingNotification);
+    };
+  }, [socket, userTyping, selectedConversation]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -14,27 +47,39 @@ const MessageInput = () => {
   };
 
   return (
-    <form className="px-4 my-3" onSubmit={handleSubmit}>
-      <div className="w-full relative">
-        <input
-          type="text"
-          className=" bg-white opacity-60 text-sm rounded-lg block w-full p-2.5 bg-inherit border-gray-600 text-black outline-none"
-          placeholder="Send a message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="absolute inset-y-0 end-0 flex items-center pe-3"
-        >
-          {loading ? (
-            <div className="loading loading-spinner"></div>
-          ) : (
-           <BsSend className="w-5 h-5 text-gray-800 hover:text-blue-500" />
-          )}
-        </button>
+    <div className="flex flex-col ">
+      <div className="mx-auto h-10 text-xs italic pt-3">
+        {userTyping == selectedConversation._id ? (
+          <>
+            <span>{selectedConversation.firstname} is typing</span>
+            <span className="loading loading-dots loading-xs"></span>
+          </>
+        ) : (
+          ""
+        )}
       </div>
-    </form>
+      <form className="px-4 my-3 " onSubmit={handleSubmit}>
+        <div className="w-full relative">
+          <input
+            type="text"
+            className=" bg-white opacity-60 text-sm rounded-lg block w-full p-2.5 bg-inherit border-gray-600 text-black outline-none"
+            placeholder="Send a message"
+            value={message}
+            onChange={userWriting}
+          />
+          <button
+            type="submit"
+            className="absolute inset-y-0 end-0 flex items-center pe-3"
+          >
+            {loading ? (
+              <div className="loading loading-spinner"></div>
+            ) : (
+              <BsSend className="w-5 h-5 text-gray-800 hover:text-blue-500" />
+            )}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 export default MessageInput;
