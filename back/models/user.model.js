dbf951f42d5e4f159ba4bfa0419c79a84505290f
Saleh <39927806+habtor@users.noreply.github.com>
2024-04-07 19:46:38 +0200

    added contact array to user model

--- back/models/user.model.js
+++ back/models/user.model.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
     profilePic: { type: String, default: "" },
+    contacts: [{ type: mongoose.Types.ObjectId, ref: "User" }],
   },
   { timestamps: true }
 );
